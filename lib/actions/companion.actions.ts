076e93a21a1951e52f631f4abd15d6d2a7957e0e
Nischal <nyyscal541556@gmail.com>
2025-05-26 18:02:51 +0545

    VAPI Setup

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -33,3 +33,13 @@ else if(topic){
 
   return companions
 }
+
+export const getCompanion = async(id:string)=>{
+  const supabase = createSupabaseClient()
+
+  const {data,error} = await supabase.from("companions").select().eq("id",id)
+
+  if(error) return console.log(error)
+
+    return data[0]
+}
