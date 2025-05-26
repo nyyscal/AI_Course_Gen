3ca3f059862b267e0ebad147decaab8a30684890
Nischal <nyyscal541556@gmail.com>
2025-05-26 18:03:03 +0545

    VAPI Setup 2.0

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -11,26 +11,29 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async({params}:CompanionSessionPageProps) => {
   const {id} = await params
-  const companion = await getCompanion(id)
+  const {name,subject,title,topic,duration} = await getCompanion(id)
   const user = await currentUser()
 
   if(!user) redirect("/sign-in")
-  if(!companion) redirect("/companions")
+  if(!name) redirect("/companions")
 
   return (
     <main>
       <article className='flex rounded-border justify-between p-6 max-md:flex-col'>
         <div className='flex items-center gap-2'>
-          <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{backgroundColor:getSubjectColor(companion.subject)}}>
-            <Image src={`/icons/${companion.subject}.svg`} alt={companion.subject} width={35} height={35}/>
+          <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{backgroundColor:getSubjectColor(subject)}}>
+            <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35}/>
           </div>
           <div className='flex flex-col gap-2'>
             <div className='flex items-center gap-2'>
-              <p className='font-bold text-2xl'>{companion.name}</p>
-              <div className='subject-badge max-sm:hidden'>{companion.subject}</div>
+              <p className='font-bold text-2xl'>{name}</p>
+              <div className='subject-badge max-sm:hidden'>{subject}</div>
             </div>
-            <p className='text-lg '>{companion.topic}</p>
+            <p className='text-lg '>{topic}</p>
           </div>
+        </div>
+        <div className='items-start text-2xl max-md:hidden'>
+        {duration} min
         </div>
       </article>
     </main>
